85aed1dd580bc000559da64657b3fcbdb54d4b9b
FWC-9696 <FWC@FWC.com>
2024-05-07 22:12:15 -0400

    Updated for PowerShell 7

--- scripts/zzz_01_Install_Software.ps1
+++ scripts/zzz_01_Install_Software.ps1
@@ -3,9 +3,8 @@
 
 Write-Host "Installing Software"
 $apps = @(
-
-
     #Microsoft Tools & Apps
+    "Microsoft.PowerShell"     #Latest Version of PowerShell
     "9NBLGGH4NNS1"             #Update Winget itself
     "Microsoft.PowerToys"      #PowerToys
     "9P7KNL5RWT25"             #Microsoft Sysinternals Suite (Includes Process explorer & Other Tools)
@@ -15,6 +14,7 @@ $apps = @(
     "9MZ95KL8MR0L"             #Snip and Sketch (New Snipping Tool)
     "9N8WTRRSQ8F7"             #Diagnostic Data Viewer
     "9WZDNCRFJBH4"             #Microsoft Photos
+    "9N8G5RFZ9XK3"             #Windows Terminal Preview
     #"9WZDNCRFJ3P2"             #Windows Movies & TV
     #"9N95Q1ZZPMH4"             #MPEG-2 Video Extension
     #"9NBLGGH10PG8"             #Microsoft People
